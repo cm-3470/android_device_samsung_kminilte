f76239e5e3d4940135950d05e14b43c48347e383
tobigun <hennymcc@yahoo.de>
2015-04-07 20:50:00 +0200

    device.mk: add OTG enabled flag

--- device.mk
+++ device.mk
@@ -96,6 +96,9 @@ PRODUCT_PACKAGES += \
     AriesParts \
     DockAudio
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.isUsbOtgEnabled=true
+
 # Dalvik VM specific for devices with 2048 MB of RAM (G800F has 1.5G, but 2G config seems to fit)
 $(call inherit-product-if-exists, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
