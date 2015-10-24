3c67472c196cf14f91ac719421e3d92a7333a0b1
tobigun <hennymcc@yahoo.de>
2015-10-24 16:23:29 +0200

    fix init
    Change-Id: I1840f5fd84d4683e571dacbc407bab7866e53da7

--- init/Android.mk
+++ init/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
-LOCAL_SRC_FILES := init_kminilte.c
+LOCAL_SRC_FILES := init_kminilte.cpp
 LOCAL_MODULE := libinit_kminilte
 
 include $(BUILD_STATIC_LIBRARY)
