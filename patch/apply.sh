86ccfae6fd74a4f2bd5799fae550a53f18c52a00
tobigun <hennymcc@yahoo.de>
2016-03-01 22:03:31 +0100

    patcher: remove check for stock or TWRP recovery
    Change-Id: Iad65382b69ef76f9aad634121e57708b122ba704

--- patch/apply.sh
+++ patch/apply.sh
@@ -5,26 +5,15 @@ PATCHBASE=$(dirname "$MYABSPATH")
 CMBASE=$(readlink -f "$PATCHBASE/../../../../")
 
 for i in $(find "$PATCHBASE"/* -type d); do
-	PATCHNAME=$(basename "$i")
-	PATCHTARGET=$PATCHNAME
-	for j in $(seq 4); do
-		PATCHTARGET=$(echo $PATCHTARGET | sed 's/_/\//')
-		if [ -d "$CMBASE/$PATCHTARGET" ]; then break; fi
-	done
+    PATCHNAME=$(basename "$i")
+    PATCHTARGET=$PATCHNAME
+    for j in $(seq 4); do
+        PATCHTARGET=$(echo $PATCHTARGET | sed 's/_/\//')
+        if [ -d "$CMBASE/$PATCHTARGET" ]; then break; fi
+    done
 
-	if [ "$PATCHTARGET" = "bootable/recovery" ]; then
-        # recovery might be replaced by TWRP -> only patch if CM recovery is used
-        cd $CMBASE/bootable/recovery
-        recovery_url=`git config --get remote.github.url`
-        cd -
-        if [ ! "$recovery_url" = "https://github.com/CyanogenMod/android_bootable_recovery" ]; then
-            echo "WARN: Recovery does not seem to be CM one -> patches will not be applied"
-            continue
-        fi
-    fi
-
-	echo
-	echo applying $PATCHNAME to $PATCHTARGET
-	cd "$CMBASE/$PATCHTARGET" || exit 1
-	git am --ignore-whitespace -3 "$PATCHBASE/$PATCHNAME"/*.patch || exit 1
+    echo
+    echo applying $PATCHNAME to $PATCHTARGET
+    cd "$CMBASE/$PATCHTARGET" || exit 1
+    git am --ignore-whitespace -3 "$PATCHBASE/$PATCHNAME"/*.patch || exit 1
 done
