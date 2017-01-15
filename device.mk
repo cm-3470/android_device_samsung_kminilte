23856ac8ec7b613597c36ed29a767b697b5f1bc9
Tobias Gunkel <hennymcc@yahoo.de>
2017-01-15 01:26:51 +0100

    codecs: update to MM (G800FXXU1CPK5)
    Change-Id: I8938c6c9ca037a522869eca2305a691937e94525

--- device.mk
+++ device.mk
@@ -81,6 +81,7 @@ PRODUCT_PACKAGES += \
 # Media config
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/media/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/media/media_profiles.xml:system/etc/media_profiles.xml
 
 # NFC
