9bec624e3d2aa6e859d5b78acd024b92c069a26f
Tobias Gunkel <hennymcc@yahoo.de>
2017-09-09 09:43:36 +0200

    update to lineageos-15.0
    Change-Id: Iccf61fb8bbdfabb5454de2d12b3d4cfb8b753268

--- lineage.mk
+++ lineage.mk
@@ -1,5 +1,5 @@
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 $(call inherit-product, device/samsung/kminilte/full_kminilte.mk)
 
 PRODUCT_NAME := lineage_kminilte
